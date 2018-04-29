14d4dd316c204934d095679b30f04b6164233c17
Enav Dachbash <38708366+EnavDachbash@users.noreply.github.com>
2018-04-29 17:28:17 +0300

    update - install filebeat

--- BasicTaskConfig/apache.sh
+++ BasicTaskConfig/apache.sh
@@ -11,3 +11,17 @@ EOD
 # Log the X-Forwarded-For
   perl -pi -e  's/^LogFormat "\%h (.* combined)$/LogFormat "%h %{X-Forwarded-For}i $1/' /etc/apache2/apache2.conf
   /usr/sbin/service apache2 restart
+
+
+# Install & Configure FileBeat 
+  wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+  sudo  /usr/bin/apt-get install apt-transport-https
+  echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+  sudo  /usr/bin/apt-get update 
+  sudo  /usr/bin/apt-get install filebeat
+  sudo update-rc.d filebeat defaults 95 10
+
+  sudo mv /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.backup
+  sudo cp -f /vagrant/filebeat.yml /etc/filebeat
+  sudo service filebeat start
+  
