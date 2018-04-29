a6c18063f28ce7db74be3d19bcb87302ff03749b
Enav Dachbash <38708366+EnavDachbash@users.noreply.github.com>
2018-04-29 17:28:38 +0300

    Update apache.sh

--- BasicTaskConfig/apache.sh
+++ BasicTaskConfig/apache.sh
@@ -16,7 +16,7 @@ EOD
 # Install & Configure FileBeat 
   wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
   sudo  /usr/bin/apt-get install apt-transport-https
-  echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+  sudo echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
   sudo  /usr/bin/apt-get update 
   sudo  /usr/bin/apt-get install filebeat
   sudo update-rc.d filebeat defaults 95 10
